f67161e9727e308790e3d60c56c5833082ec54ae
Renato Serafim <renatoserafim719@gmail.com>
2025-02-25 16:35:03 -0300

    aula de php com exemplos de concateação de variaveis

---
+++ aula/olá.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documento</title>
+</head>
+<body>
+    <?php
+    echo "hello world <br>";
+
+    $variavel = "Renato";
+    $numero = "4";
+    $sobrenome = "Serafim";
+
+    echo "${variavel} <br>";
+    echo "${numero} <br>";
+    echo "${sobrenome} <br>";
+    echo "a";
+    ?>
+</body>
+</html>
