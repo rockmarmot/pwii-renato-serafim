ec3f965718b3c571d1e185c0db05d78a9c1813eb
Renato Serafim <rentoserafim719@gmail.com>
2025-04-29 16:31:39 -0300

    laços

---
+++ aula04/lacos.php
@@ -0,0 +1,21 @@
+<?php
+
+$contador = 1;
+
+while ($contador < 6) {
+    echo $contador, "<br>";
+    $contador++;
+}
+
+$contadorii = 1;
+
+do {
+    echo $contadorii, "<br>";
+    $contadorii++;
+} while ($contadorii < 6 );
+
+for ($x = 0; $x <= 6; $x++){
+    echo $x, "<br>";
+}
+
+?>
