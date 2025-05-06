ad6ec06a3d0ea276fea1adc8d58afa389f7b13d9
Renato Serafim <renatoserafim719@gmail.com>
2025-05-06 16:35:29 -0300

    usando label e index

---
+++ aula05/index.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="manssagem.php" method="POST">
+           <label for="msg">Digite sua menssagem:</label> 
+            <input type="text" name="msg"><br>
+            <input type="submit">
+
+        </form>
+    </body>
+</html>
---
+++ aula05/mensagem.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1> <?php echo $_POST["msg"]; ?> </h1>
+    </body>
+</html>
